3579b1c9f4b91bd57be1349e5acecca01d4cdeee
Adophilus <uchenna19of@gmail.com>
2022-11-15 09:28:40 +0100

    adjusted the page header to a suitable format

--- components/admin/dashboard/elements/pageHeader.jsx
+++ components/admin/dashboard/elements/pageHeader.jsx
@@ -4,17 +4,15 @@ export default function PageHeaderComponent({ title, children }) {
       <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
         <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
           <div className="flex-1 min-w-0">
-            <div className="flex items-center">
+            <div className="flex justify-between">
               <div>
                 <div className="flex items-center">
                   <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
-                    <>
-                      {title}
-                      {children}
-                    </>
+                    {title}
                   </h1>
                 </div>
               </div>
+              <div>{children}</div>
             </div>
           </div>
         </div>
