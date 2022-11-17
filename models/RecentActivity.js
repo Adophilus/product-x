22087a7e64e2d11aa8dd5f1f35074f8d561f15c1
Adophilus <uchenna19of@gmail.com>
2022-11-17 07:11:20 +0100

    added RecentActivity model

---
+++ models/RecentActivity.js
@@ -0,0 +1,22 @@
+import connection from '@/utils/db'
+import mongoose from 'mongoose'
+
+export default connection.models.Track ??
+  connection.model(
+    'RecentActivity',
+    mongoose.Schema({
+      operation: {
+        type: String,
+        required: true
+      },
+      status: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: String,
+        required: true,
+        default: Date.now
+      }
+    })
+  )
