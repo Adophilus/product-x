7c10a6cb69ffbdef7ecd2459786707dea5a6ecb4
Adophilus <uchenna19of@gmail.com>
2022-11-17 07:37:12 +0100

    added form

--- components/admin/dashboard/elements/button.jsx
+++ components/admin/dashboard/elements/button.jsx
@@ -1,8 +1,14 @@
-export default function ButtonComponent({ children }) {
+import { classNames } from '@/utils/helpers'
+
+export default function ButtonComponent({ children, onClick, className }) {
   return (
     <button
+      onClick={onClick}
       type="button"
-      className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-cyan-100 bg-cyan-700 hover:bg-cyan-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+      className={classNames(
+        className,
+        'inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500'
+      )}
     >
       {children}
     </button>
--- pages/admin/dashboard/tracks.jsx
+++ pages/admin/dashboard/tracks.jsx
@@ -1,5 +1,5 @@
 import Layout from '@/components/admin/dashboard/layout'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import useSWR from 'swr'
 import { PlusIcon, UserIcon, BookOpenIcon } from '@heroicons/react/outline'
 import Button from '@/components/admin/dashboard/elements/button'
@@ -23,27 +23,110 @@ const tableHeaders = [
   },
   { name: 'Date Added' }
 ]
-/*
-const tracks = [
-  [
-    <div className="flex">
-      <a
-        href={'/admin/dashboard/tracks/data-science'}
-        className="group inline-flex space-x-2 truncate text-sm"
-      >
-        <p className="text-gray-500 truncate group-hover:text-gray-900">
-          Data Science
-        </p>
-      </a>
-    </div>,
-    <span className="flex text-gray-500">10</span>,
-    <time className="text-gray-500" dateTime={formatEpoch(1668502165546)}>
-      {formatEpoch(1668502165546)}
-    </time>
-  ]
-]
-*/
+
+function AddTrackForm() {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col mt-2">
+          <div className="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
+            <form className="space-y-8 divide-y divide-gray-200">
+              <div className="space-y-8 divide-y divide-gray-200">
+                <div>
+                  <div>
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+                      Profile
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                      This information will be displayed publicly so be careful
+                      what you share.
+                    </p>
+                  </div>
+
+                  <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="track-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Track name
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          name="track-name"
+                          id="track-name"
+                          className="shadow-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="track-link"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Track link
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          name="track-link"
+                          id="track-link"
+                          autoComplete="family-name"
+                          className="shadow-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-6">
+                      <label
+                        htmlFor="about"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Description
+                      </label>
+                      <div className="mt-1">
+                        <textarea
+                          id="about"
+                          name="about"
+                          rows={3}
+                          className="shadow-sm focus:ring-cyan-500 focus:border-cyan-500 block w-full sm:text-sm border border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <p className="mt-2 text-sm text-gray-500">
+                        Write some information about the track
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-5">
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
 export default function TracksView() {
+  const [isAddingTrack, setIsAddingTrack] = useState(false)
   const fetcher = (...args) => fetch(...args).then((res) => res.json())
   const { data, error } = useSWR('/api/tracks', fetcher)
   const tracks =
@@ -81,7 +164,11 @@ export default function TracksView() {
       icon={BookOpenIcon}
       pageHeader={
         <PageHeaderComponent icon={BookOpenIcon} title={'Tracks'}>
-          <Button>
+          <Button
+            onClick={() =>
+              setIsAddingTrack((_isAddingTrack) => !_isAddingTrack)
+            }
+          >
             Add Track
             <PlusIcon className="ml-3 -mr-1 h-5 w-5" aria-hidden="true" />
           </Button>
@@ -89,6 +176,7 @@ export default function TracksView() {
       }
       breadcrumbs={breadcrumbs}
     >
+      {isAddingTrack && <AddTrackForm />}
       <TableComponent headers={tableHeaders} records={tracks} />
     </Layout>
   )
