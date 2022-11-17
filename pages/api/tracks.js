c448e04550cd5dd106d25210da7dc86943560acb
Adophilus <uchenna19of@gmail.com>
2022-11-17 04:21:32 +0100

    made the tracks api a little more readable

--- pages/api/tracks.js
+++ pages/api/tracks.js
@@ -1,21 +1,26 @@
 import Track from '@/models/Track'
-import {ReasonPhrases, StatusCodes} from 'http-status-codes'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 
-export default async function handler (req, res) {
+export default async function handler(req, res) {
   switch (req.method) {
     case 'GET':
-      return res.status(StatusCodes.OK).send(await Track.find())
+      const tracks = await Track.find()
+      console.log(tracks)
+      return res.status(StatusCodes.OK).send(tracks)
     case 'PUT':
       const trackName = req.body.name
       try {
         const track = await Track.create({ name: trackName })
         return res.status(StatusCodes.CREATED).send(track)
-      }
-      catch (err) {
+      } catch (err) {
         console.log(err)
-        return res.send(StatusCodes.INTERNAL_SERVER_ERROR).send({ error: ReasonPhrases.INTERNAL_SERVER_ERROR })
+        return res
+          .send(StatusCodes.INTERNAL_SERVER_ERROR)
+          .send({ error: ReasonPhrases.INTERNAL_SERVER_ERROR })
       }
     default:
-      return res.status(StatusCodes.METHOD_NOT_ALLOWED).send({ error: ReasonPhrases.METHOD_NOT_ALLOWED })
+      return res
+        .status(StatusCodes.METHOD_NOT_ALLOWED)
+        .send({ error: ReasonPhrases.METHOD_NOT_ALLOWED })
   }
 }
