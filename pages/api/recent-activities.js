82f032862e83913b874c85308aed4ccee1039f35
Adophilus <uchenna19of@gmail.com>
2022-11-17 07:15:55 +0100

    fixed typo

--- pages/api/recent-activities.js
+++ pages/api/recent-activities.js
@@ -1,11 +1,10 @@
 import RecentActivity from '@/models/RecentActivity'
-import Track from '@/models/Track'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 
 export default async function handler(req, res) {
   switch (req.method) {
     case 'GET':
-      const recentActivities = await recentActivities.find()
+      const recentActivities = await RecentActivity.find()
       return res.status(StatusCodes.OK).send(recentActivities)
     default:
       return res
