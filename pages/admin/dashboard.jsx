d8a4ea8ef27b9feb6ec67f08924968b415c03ded
Adophilus <uchenna19of@gmail.com>
2022-11-17 07:11:44 +0100

    added api endpoint for recent activity

--- pages/admin/dashboard.jsx
+++ pages/admin/dashboard.jsx
@@ -5,16 +5,12 @@ import { ChevronRightIcon } from '@heroicons/react/solid'
 import { classNames } from '@/utils/helpers'
 import PageHeaderComponent from '@/components/admin/dashboard/elements/pageHeader'
 import useSWR from 'swr'
+import TableComponent from '@/components/admin/dashboard/elements/table'
 
-const recentActivities = [
-  {
-    id: 1,
-    name: 'Added Data Science track',
-    href: '#',
-    status: 'success',
-    date: 'July 11, 2020',
-    datetime: '2020-07-11'
-  }
+const tableHeaders = [
+  { name: 'Operation' },
+  { name: 'Status' },
+  { name: 'Date' }
 ]
 
 const statusStyles = {
@@ -26,6 +22,52 @@ const statusStyles = {
 const breadcrumbs = [
   { name: 'Dashboard', path: '/admin/dashboard', current: false }
 ]
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+function RecentActivities() {
+  const { data, error } = useSWR('/api/recent-activities', fetcher)
+  const recentActivities =
+    error != null
+      ? []
+      : data == null
+      ? []
+      : data.map((recentActivity) => [
+          <div key={recentActivity.id} className="flex">
+            <a
+              href={`/admin/dashboard/recent-activities/${recentActivity.id}`}
+              className="group inline-flex space-x-2 truncate text-sm"
+            >
+              <p className="text-gray-500 truncate group-hover:text-gray-900">
+                {recentActivity.operation}
+              </p>
+            </a>
+          </div>,
+          <span
+            className={classNames(
+              statusStyles[recentActivity.status],
+              'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium capitalize'
+            )}
+          >
+            {recentActivity.status}
+          </span>,
+          <time
+            className="text-gray-500"
+            dateTime={formatEpoch(recentActivity.date)}
+          >
+            {formatEpoch(recentActivity.date)}
+          </time>
+        ])
+  return (
+    <>
+      <h2 className="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
+        Recent activity
+      </h2>
+
+      <TableComponent records={recentActivities} headers={tableHeaders} />
+    </>
+  )
+}
 
 function Card({ card }) {
   return (
@@ -64,7 +106,6 @@ function Card({ card }) {
 }
 
 function Cards() {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json())
   const { data, error } = useSWR('/api/overview', fetcher)
   console.log(data)
   const cards =
@@ -126,149 +167,7 @@ export default function DashboardView() {
           <Cards />
         </div>
 
-        <h2 className="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
-          Recent activity
-        </h2>
-
-        {/* Activity list (smallest breakpoint only) */}
-        <div className="shadow sm:hidden">
-          <ul
-            role="list"
-            className="mt-2 divide-y divide-gray-200 overflow-hidden shadow sm:hidden"
-          >
-            {recentActivities.map((recentActivity) => (
-              <li key={recentActivity.id}>
-                <a
-                  href={recentActivity.href}
-                  className="block px-4 py-4 bg-white hover:bg-gray-50"
-                >
-                  <span className="flex items-center space-x-4">
-                    <span className="flex-1 flex space-x-2 truncate">
-                      <span className="flex flex-col text-gray-500 text-sm truncate">
-                        <span className="truncate">{recentActivity.name}</span>
-                        <time dateTime={recentActivity.datetime}>
-                          {recentActivity.date}
-                        </time>
-                      </span>
-                    </span>
-                    <ChevronRightIcon
-                      className="flex-shrink-0 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </a>
-              </li>
-            ))}
-          </ul>
-
-          <nav
-            className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200"
-            aria-label="Pagination"
-          >
-            <div className="flex-1 flex justify-between">
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-              >
-                Previous
-              </a>
-              <a
-                href="#"
-                className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-              >
-                Next
-              </a>
-            </div>
-          </nav>
-        </div>
-
-        {/* Activity table (small breakpoint and up) */}
-        <div className="hidden sm:block">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col mt-2">
-              <div className="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead>
-                    <tr>
-                      <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Operation
-                      </th>
-                      <th className="hidden px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider md:block">
-                        Status
-                      </th>
-                      <th className="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Date
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {recentActivities.map((recentActivity) => (
-                      <tr key={recentActivity.id} className="bg-white">
-                        <td className="max-w-0 w-full px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                          <div className="flex">
-                            <a
-                              href={recentActivity.href}
-                              className="group inline-flex space-x-2 truncate text-sm"
-                            >
-                              <p className="text-gray-500 truncate group-hover:text-gray-900">
-                                {recentActivity.name}
-                              </p>
-                            </a>
-                          </div>
-                        </td>
-                        <td className="hidden px-6 py-4 whitespace-nowrap text-sm text-gray-500 md:block">
-                          <span
-                            className={classNames(
-                              statusStyles[recentActivity.status],
-                              'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium capitalize'
-                            )}
-                          >
-                            {recentActivity.status}
-                          </span>
-                        </td>
-                        <td className="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                          <time dateTime={recentActivity.datetime}>
-                            {recentActivity.date}
-                          </time>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                {/* Pagination */}
-                <nav
-                  className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
-                  aria-label="Pagination"
-                >
-                  <div className="hidden sm:block">
-                    <p className="text-sm text-gray-700">
-                      Showing <span className="font-medium">1</span> to{' '}
-                      <span className="font-medium">10</span> of{' '}
-                      <span className="font-medium">
-                        {recentActivities.length}
-                      </span>{' '}
-                      results
-                    </p>
-                  </div>
-                  <div className="flex-1 flex justify-between sm:justify-end">
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                    >
-                      Previous
-                    </a>
-                    <a
-                      href="#"
-                      className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                    >
-                      Next
-                    </a>
-                  </div>
-                </nav>
-              </div>
-            </div>
-          </div>
-        </div>
+        <RecentActivities />
       </div>
     </Layout>
   )
