ba3d7f2e575b0ca0496da6f5f7ee1ad787bb1795
Adophilus <uchenna19of@gmail.com>
2022-11-21 22:05:53 +0100

    changed the color scheme of the button component

--- components/admin/dashboard/elements/button.jsx
+++ components/admin/dashboard/elements/button.jsx
@@ -7,7 +7,7 @@ export default function ButtonComponent({ children, onClick, className }) {
       type="button"
       className={classNames(
         className,
-        'inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500'
+        'inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500'
       )}
     >
       {children}
