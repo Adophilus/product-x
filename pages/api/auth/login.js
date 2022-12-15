ec45835ccea3dde49c4fd745a32859d6f109ca13
Adophilus <uchenna19of@gmail.com>
2022-12-15 18:10:04 +0100

    added new endpoint for email login

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -2,14 +2,22 @@ import User from '@/models/User'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import jwt from 'jsonwebtoken'
 import config from '@/config'
+import email from '@/services/email'
 
 export default async function handler(req, res) {
   switch (req.method) {
     case 'POST':
-      // send login mail to user
+      const link = LoginLink.create()
+      email.send({
+        template: 'login',
+        message: {
+          to: req.body.email
+        },
+        locals: link
+      })
       break
     case 'PATCH':
-      const user = User.findOne({ email: req.body.email })
+      const user = await User.findOne({ email: req.body.email })
       if (!user)
         return res
           .status(StatusCodes.UNAUTHORIZED)
