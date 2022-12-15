578509fefa46d5b39440338ef6d99aad0c624116
Adophilus <uchenna19of@gmail.com>
2022-12-15 17:48:53 +0100

    refactored configs

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
+import mailConfig from '@/config/mail'
 import getConfig from 'next/config'
 
 const { serverRuntimeConfig } = getConfig()
 
 export default {
-  secret: serverRuntimeConfig.secret
+  secret: serverRuntimeConfig.secret,
+  mail: mailConfig
 }
---
+++ config/mail.js
@@ -0,0 +1,10 @@
+export default {
+  host: '127.0.0.1',
+  port: 1025,
+  ssl: false,
+  tls: true,
+  auth: {
+    user: 'product-x',
+    pass: 'product-x'
+  }
+}
