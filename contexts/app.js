d924b0c2782709dcbfe5b967b28c56ab25341974
Adophilus <uchenna19of@gmail.com>
2022-12-15 05:10:38 +0100

    modded the state of the app context

--- contexts/app.js
+++ contexts/app.js
@@ -2,8 +2,16 @@ import { createContext } from 'react'
 
 export const AppContext = createContext({})
 
+const initialState = {
+  user: {
+    email: 'test@mail.com',
+    profileImg:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  }
+}
+
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{ user: null }}>
-    {children}
-    </AppContext.Provider>
+  return (
+    <AppContext.Provider value={initialState}>{children}</AppContext.Provider>
+  )
 }
