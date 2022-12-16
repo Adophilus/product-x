1ca01a7893d98ab9158b7887cf8111d627843154
Adophilus <uchenna19of@gmail.com>
2022-12-16 11:15:40 +0100

    removed provider from index.js

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import Head from 'next/head'
 import Landpage from '../components/Landpage'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { AppProvider } from '@/contexts/app'
 
 export default function Home() {
   return (
-    <AppProvider>
       <main>
         <Head>
           <title>Product-X</title>
@@ -17,6 +15,5 @@ export default function Home() {
         <Landpage />
         <Footer />
       </main>
-    </AppProvider>
   )
 }
